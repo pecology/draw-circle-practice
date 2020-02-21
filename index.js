89832bb2922377a4fc542f70059093ce963f4350
pecology <dummy>
2020-02-22 01:32:13 +0900

    unfix center point

--- index.js
+++ index.js
@@ -16,8 +16,9 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
 //中心
-const centerPoint = new Point(250, 250);
-ctx.fillRect(centerPoint.x - 5, centerPoint.y -5, 10, 10);
+ctx.beginPath();
+const globalCenterPoint = new Point(250, 250);
+ctx.fillRect(globalCenterPoint.x - 5, globalCenterPoint.y -5, 10, 10);
 
 ctx.lineWidth = 5;
 ctx.lineCap = "round";
@@ -28,14 +29,14 @@ let mousedown = false;
 onStart = point => {
     document.querySelector("#score").innerText = "";
     ctx.clearRect(0, 0, 500, 500);
-    const centerPoint = new Point(250, 250);
-    ctx.fillRect(centerPoint.x - 5, centerPoint.y -5, 10, 10);
+    if(document.querySelector("#fix-center-point").checked) {
+        const centerPoint = new Point(250, 250);
+        ctx.fillRect(centerPoint.x - 5, centerPoint.y -5, 10, 10);
+    }
 
     ctx.beginPath();
     ctx.strokeStyle = "Black";
     ctx.globalAlpha =  1;
-    // ctx.moveTo(e.offsetX, e.offsetY);
-    // drawedPoints.push(new Point(e.offsetX, e.offsetY));
     ctx.moveTo(point.x, point.y);
     drawedPoints.push(new Point(point.x, point.y));
     mousedown = true;
@@ -52,6 +53,14 @@ onMove = point => {
 onEnd = () => {
     mousedown = false;
 
+    let centerPoint
+    if(document.querySelector("#fix-center-point").checked) {
+        centerPoint = globalCenterPoint;
+    } else {
+        centroidX = drawedPoints.reduce((prev, curr) => prev + curr.x, 0) / drawedPoints.length;
+        centroidY = drawedPoints.reduce((prev, curr) => prev + curr.y, 0) / drawedPoints.length;
+        centerPoint = new Point(centroidX, centroidY);
+    }
     const distancesToCenter = drawedPoints.map(p => p.distance(centerPoint));
     const radius = distancesToCenter.reduce((prev, curr) => prev + curr, 0) / distancesToCenter.length;
 
